bea5ff29bfed3b45ec953907a9993d8426c6b783
Simonas Danielius <s.danielius8@gmail.com>
2026-04-04 19:26:10 +0300

    Small bugfix

--- backend/job-board-api/job-board-api/Program.cs
+++ backend/job-board-api/job-board-api/Program.cs
@@ -67,7 +67,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins(
+                "http://localhost:5173",
+                "https://simasdan.github.io"
+              )
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
