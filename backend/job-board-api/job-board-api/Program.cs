f0ff81b020e429eb7b47790eba715fc1019d416f
Simonas Danielius <s.danielius8@gmail.com>
2026-04-04 14:10:15 +0300

    Docker setup

--- backend/job-board-api/job-board-api/Program.cs
+++ backend/job-board-api/job-board-api/Program.cs
@@ -100,21 +100,22 @@ builder.Services.AddTransient<JobApplicationService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Job Board API v1");
-    });
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Job Board API v1");
+});
 
-app.UseHttpsRedirection();
 app.UseCors("AllowReactApp");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 AppDbInitializer.Seed(app);
 app.Run();
